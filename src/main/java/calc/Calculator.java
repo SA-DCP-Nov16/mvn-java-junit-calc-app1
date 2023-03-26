8c60e2359edeb02fca2516870eb0b68c765c0326
Loksai <loksai.eta@gmail.com>
2023-03-26 06:43:07 +0000

    Initial Commit

---
+++ src/main/java/calc/Calculator.java
@@ -0,0 +1,21 @@
+package calc;
+
+public class Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a - b;  // ERROR!!!
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+}
---
+++ src/test/java/calc/CalculatorTest.java
@@ -0,0 +1,37 @@
+package calc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSum() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.sum(2, 2);
+		// Then
+		if (result != 4) {   // if 2 + 2 != 4
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testMinus() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(0, calculator.minus(2, 2));
+	}
+
+	@Test
+	public void testDivide() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(2, calculator.divide(6, 3));
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testDivideWillThrowExceptionWhenDivideOnZero() {
+		Calculator calculator = new Calculator();
+		calculator.divide(6, 0);
+	}
+}
